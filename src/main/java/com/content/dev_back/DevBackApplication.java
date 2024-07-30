418d4e25323ac169a430e6953124b3f81cd1606d
종웅 <t1t600@gmail.com>
2024-07-30 22:45:22 +0900

    initialspring

---
+++ src/main/java/com/content/dev_back/DevBackApplication.java
@@ -0,0 +1,13 @@
+package com.content.dev_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/content/dev_back/DevBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.content.dev_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
